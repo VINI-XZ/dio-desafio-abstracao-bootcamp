bb1b9dd364ce63583f610a6cf19f5a0a37fac01c
Vinicius Xavier Cruz <viniciusxavier45@hotmail.com>
2024-07-25 23:28:40 -0300

    Projeto concluido

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -17,18 +19,39 @@ public class Main {
 		curso2.setDescricao("descrição curso js");
 		curso2.setCargaHoraria(4);
 		
-		System.out.println(curso1);
-		System.out.println(curso2);
+		
 		
 		Mentoria mentoria = new Mentoria();
 		mentoria.setTitulo("mentoria de java");
 		mentoria.setDescricao("descrição mentoria java");
 		mentoria.setData(LocalDate.now());
+	
 		
-		System.out.println(mentoria);
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev devVini = new Dev();
+		devVini.setNome("Vinicius");
+		devVini.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos inscritos Vinicius "+ devVini.getConteudosInscritos());
+		devVini.progredir();
+		System.out.println("Conteúdos concluidos Vinicius "+ devVini.getConteudosConcluidos());
+		System.out.println("XP: " + devVini.calcularTotalXp());
+		
+		
+		
+		Dev devRadagon = new Dev();
+		devRadagon.setNome("Radagon");
+		devRadagon.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos inscritos Radagon "+ devRadagon.getConteudosInscritos());
+		devRadagon.progredir();
+		System.out.println("Conteúdos concluidos Vinicius "+ devVini.getConteudosConcluidos());
+		System.out.println("XP: " + devRadagon.calcularTotalXp());
 
-		
-		
 	}
 
 }
